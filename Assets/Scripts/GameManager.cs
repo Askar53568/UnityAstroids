7c079d4979d8dc46ba36e3363b4b490111e408e6
Askar53568 <diana.suankulova@gmail.com>
2022-04-19 12:33:30 +0100

    Added player invincibility after respawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour {
     public Player player;
     //Time after which the player can respawn
     public float respawnTime = 2.0f;
+    //Time for which a player cannot collide with any objects in the scene
+    public float respawnInvisibility = 3.0f;
 
     public int lives = 3;
 
@@ -26,6 +28,16 @@ public class GameManager : MonoBehaviour {
         this.player.transform.position = Vector3.zero;
         //Re-activate the player game object
         this.player.gameObject.SetActive(true);
+        //Change the layer of the player object to Respawn which will ignore all collisions
+        this.player.gameObject.layer = LayerMask.NameToLayer("Respawn");
+        //After 3 seconds turn on collisions
+        Invoke(nameof(TurnOnCollisions), this.respawnInvisibility);
+    }
+
+    //Reset the player object layer back to having normal collisions (asteroids)
+    private void TurnOnCollisions()
+    {
+        this.player.gameObject.layer = LayerMask.NameToLayer("Player");
     }
 
     private void GameOver()
