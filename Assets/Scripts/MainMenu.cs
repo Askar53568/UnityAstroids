c3df7e5e017409adb6b82e501148568689dc034f
Askar53568 <diana.suankulova@gmail.com>
2022-05-11 15:40:07 +0100

    changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,22 +10,6 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("ProfilesMenu");
     }
 
-<<<<<<< HEAD
-=======
-    public void PlayButton ()
-    {
-        SceneManager.LoadScene("Game");
-    }
-    public void LoadButton ()
-    {
-        SceneManager.LoadScene("Game");
-    }
-    public void AchButton ()
-    {
-        SceneManager.LoadScene("Achievements");
-    }
-
->>>>>>> f8002301dd91344294887fe90016bdd363b0c789
     public void BackButton ()
     {
         SceneManager.LoadScene("MainMenu");
